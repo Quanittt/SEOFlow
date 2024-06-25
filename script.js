fd87de53956b13a59b16f9285fa3d3074b333dce
Maks <maksimlozinskiy12@gmail.com>
2024-06-25 13:01:12 +0300

    13:01

---
+++ script.js
@@ -0,0 +1,10 @@
+let dd_list = document.getElementById('dropdown-menu');
+let dd_btn = document.getElementById('dropdown-btn');
+
+dd_btn.addEventListener('click', function() {
+    if (window.innerWidth < 928) {
+    
+    } else {
+        
+    }
+})
