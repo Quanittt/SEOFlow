7abff34f59355c2ef8691449c9ea9bd57a6b0d70
Maks <maksimlozinskiy12@gmail.com>
2024-06-30 02:14:59 +0300

    final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let btn = document.getElementById("btn");
+
 document.addEventListener('DOMContentLoaded', function() {
     const ddBtn = document.getElementById('dropdown-btn');
     const ddList = document.getElementById('dropdown-menu');
@@ -19,4 +21,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Initial check
     handleResize();
+});
+
+btn.addEventListener("click", function() {
+    let name = document.getElementById("name").value;
+    let companyname = document.getElementById("companyname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let servica = document.getElementById("servica").value;
+    let message = document.getElementById("message").value;
+    
+    var data = JSON.stringify({
+        "name": name,
+        "companyname": companyname,
+        "email": email,
+        "phone": phone,
+        "servica": servica,
+        "message": message
+      });
+      
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = false;
+      
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+          console.log(this.responseText);
+        }
+      });
+      
+      xhr.open("POST", "https://makertime-b895.restdb.io/rest/query");
+      xhr.setRequestHeader("content-type", "application/json");
+      xhr.setRequestHeader("x-apikey", "94e241394df9f069696c9aa705c565cfcfab4");
+      xhr.setRequestHeader("cache-control", "no-cache");
+      
+      xhr.send(data);
 });
